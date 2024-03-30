0f508bc193f8036ba953ea49a89b6cfedb72376b
tanpopo <tanpopo@MacBook-Air.local>
2024-03-30 15:55:50 +0900

    くじを引く処理の調整

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function sleep(ms) {
 async function login(driver, url) {
   await console.log('ログイン処理開始')
   // ユーザー名の入力
-  await sleep(5000)
+  await sleep(2000)
   let username = await driver.findElement(By.name('username'))
   await driver.wait(until.elementIsEnabled(username), 10000)
   await driver.wait(until.elementIsVisible(username), 10000)
@@ -24,7 +24,7 @@ async function login(driver, url) {
       return await console.log('ユーザー名完了')
     })
     .then(async function() {
-      await sleep(5000)
+      await sleep(2000)
 
       let password = await driver.findElement(By.name('password'))
       await driver.wait(until.elementIsEnabled(password), 10000)
@@ -48,24 +48,24 @@ async function login(driver, url) {
   return driver;
 }
 
-async function kuji(driver, url) {
+async function kuji(driver) {
   // くじを引ける画面の場合
   await console.log(await driver.getTitle()+':くじを引く。')
 
   await driver.wait(until.elementLocated(By.id('entry')), 10000)
-  let btn = await driver.findElement(By.id('entry'))
+  let btn = null
+  btn = await driver.findElement(By.id('entry'))
   await driver.wait(until.elementIsEnabled(btn), 10000)
   await driver.wait(until.elementIsVisible(btn), 10000)
   let status = false
   while(status == false) {
     style = await btn.getAttribute('height')
-    status = style > 10
+    status = await style > 10
   }
   await btn.click()
 
   // くじを引き、リダイレクト処理が行われるまで待機する。
-  var reg = await new RegExp('^(?!.*'+url+').+$')
-  await driver.wait(until.urlMatches(reg), 20000)
+  await sleep(20000)
 
   return driver;
 }
@@ -77,26 +77,30 @@ async function kuji(driver, url) {
     urls.push(await url.getAttribute('href'))
   }
 
-  urls = [urls[0]]
-
   for (url of urls) {
     console.log(url)
     try {
       await driver.get(url)
-      current_url = driver.getCurrentUrl()
+      current_url = await driver.getCurrentUrl()
 
-      if (await driver.wait(until.urlContains('login'), 5000)) {
-        console.log('ログイン処理')
+      if (await current_url.indexOf('login') >= 0) {
         await login(driver, await current_url)
-        await kuji(driver, url)
+        await kuji(driver)
+      }
+      else if (await current_url.indexOf('pointmall') >= 0) {
+        console.log('対象外ページ')
       }
       else {
-        await kuji(driver, url)
+        await kuji(driver)
       }
     }
     catch (error) {
-      console.log(error)
-      console.log(await driver.getTitle()+':くじは引けませんでした。')
+      if (error instanceof TimeoutError) {
+        console.log('くじをすでに引いています。')
+      }
+      else {
+        console.log(await driver.getTitle()+':くじは引けませんでした。')
+      }
     }
   }
   driver.quit()
